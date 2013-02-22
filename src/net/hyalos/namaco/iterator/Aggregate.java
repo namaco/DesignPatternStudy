32fbb053ab40269fe1dfa0b345ef68de2d29d745
T.Ebina <endless.namaco@gmail.com>
2013-02-22 22:27:55 +0900

    First Commit.

---
+++ src/net/hyalos/namaco/iterator/Aggregate.java
@@ -0,0 +1,10 @@
+package net.hyalos.namaco.iterator;
+
+/**
+ * User: T.Ebina
+ * Date: 2013/02/22
+ * Time: 22:16
+ */
+public interface Aggregate {
+    public abstract Iterator iterator();
+}
---
+++ src/net/hyalos/namaco/iterator/Iterator.java
@@ -0,0 +1,11 @@
+package net.hyalos.namaco.iterator;
+
+/**
+ * User: T.Ebina
+ * Date: 2013/02/22
+ * Time: 22:22
+ */
+public interface Iterator {
+    public abstract boolean hasNext();
+    public abstract Object next();
+}
