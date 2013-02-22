526fa37376f4b7a96561907ec7bb3fe4a1129b55
T.Ebina <endless.namaco@gmail.com>
2013-02-22 22:50:23 +0900

    First Commit.

---
+++ src/net/hyalos/namaco/iterator/Book.java
@@ -0,0 +1,18 @@
+package net.hyalos.namaco.iterator;
+
+/**
+ * User: T.Ebina
+ * Date: 2013/02/22
+ * Time: 22:45
+ */
+public class Book {
+    private String name;
+
+    public Book(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
