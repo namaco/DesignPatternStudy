2dfb51f35e26063a629202a2357dd58218c43e4c
T.Ebina <endless.namaco@gmail.com>
2013-02-22 23:14:47 +0900

    First Commit.

---
+++ src/net/hyalos/namaco/iterator/BookShelfIterator.java
@@ -0,0 +1,33 @@
+package net.hyalos.namaco.iterator;
+
+/**
+ * User: T.Ebina
+ * Date: 2013/02/22
+ * Time: 22:58
+ */
+public class BookShelfIterator implements Iterator {
+    private BookShelf bookShelf;
+    private int index;
+
+    public BookShelfIterator(BookShelf bookShelf) {
+        this.bookShelf = bookShelf;
+        this.index = 0;
+    }
+
+    @Override
+    public boolean hasNext() {
+        if (index < bookShelf.getLength()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public Object next() {
+        Book book = bookShelf.getBookAt(index);
+        index++;
+        return book;
+    }
+
+}
