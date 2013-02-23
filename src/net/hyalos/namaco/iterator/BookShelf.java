0ccaa02df2838a79267cc55b6c84260b7ac263ce
T.Ebina <endless.namaco@gmail.com>
2013-02-23 15:57:01 +0900

    Changed into  ArrayList from array.

--- src/net/hyalos/namaco/iterator/BookShelf.java
+++ src/net/hyalos/namaco/iterator/BookShelf.java
@@ -1,28 +1,30 @@
 package net.hyalos.namaco.iterator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * User: T.Ebina
  * Date: 2013/02/22
  * Time: 22:50
  */
 public class BookShelf implements Aggregate {
-    private Book[] books;
+    private List<Book> bookList;
     private int last = 0;
 
-    public BookShelf(int maxsize) {
-        this.books = new Book[maxsize];
+    public BookShelf(int initialSize) {
+        this.bookList = new ArrayList(initialSize);
     }
     public Book getBookAt(int index){
-        return books[index];
+        return bookList.get(index);
     }
 
     public void appendBook(Book book) {
-        this.books[last] = book;
-        last++;
+        bookList.add(book);
     }
 
     public int getLength() {
-        return last;
+        return bookList.size();
     }
 
     @Override
