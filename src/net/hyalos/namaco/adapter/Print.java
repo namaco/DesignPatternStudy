64f4715a9dc93bf3ca103aa5c443d5baeed2823d
T.Ebina <endless.namaco@gmail.com>
2013-02-25 22:21:37 +0900

    First Commit.

---
+++ src/net/hyalos/namaco/adapter/Print.java
@@ -0,0 +1,12 @@
+package net.hyalos.namaco.adapter;
+
+/**
+ * User: T.Ebina
+ * Date: 2013/02/25
+ * Time: 22:13
+ */
+public interface Print {
+    public abstract void printWeak();
+
+    public abstract void printStrong();
+}
